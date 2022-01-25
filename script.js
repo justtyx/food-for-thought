edfa8616e5229220a6d20b3eab43a44021a76e2a
Justina Jemeljanovaite <just1528@stud.kea.dk>
2022-01-25 22:45:00 +0100

    dynamic images

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function loadText(text) {
         boxBody.innerHTML = text[i].body;
         boxText.classList.add('fade-in');
         i++;
-        console.log(i);
+        // console.log(i);
     } else if (i === 7) {
             console.log('hey');
             boxHeading.innerHTML = text[6].heading;
@@ -58,24 +58,75 @@ function loadText(text) {
             theButton.innerHTML = 'Play a game';
 
             theButton.addEventListener('click', openGameOne);
-
-    function openGameOne() {
-        theBox.classList.add('scale-up');
-        boxText.classList.add('fade-out');
-        boxHeading.innerHTML = "";
-        boxBody.innerHTML = "";
-        theButton.innerHTML = 'Done';
-        theButton.addEventListener('click', () => {
-        theBox.classList.remove('scale-up');
-            theButton.innerHTML = 'Finish';
-            boxHeading.innerHTML="Good job!"
-            boxBody.innerHTML="Select a plate to get a recipe."
-            document.querySelector('.plates').classList.remove('hidden');
-            document.querySelector('.step-two').classList.remove('inactive');
-        })
-    }
         }
 }}
+
+
+function openGameOne(gameOneData) {
+    
+// fetch food pyramid data
+fetch(`https://spring21-427e.restdb.io/rest/food-pyramid`, {
+    "method": "GET",
+    "headers": {
+      "x-apikey": "6034a7eb5ad3610fb5bb6548"
+    }
+  })
+  .then((res) => res.json())
+  .then(response => {
+    loadGameOne(response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+  function loadGameOne(gameOneData) {
+    console.log(gameOneData);
+
+    const pyramidTemplate = document.querySelector('.food-pyramid-img').content;
+    
+    gameOneData.forEach(item => {
+       
+        //clone it
+        const clone = pyramidTemplate.cloneNode(true); 
+        //change content
+        console.log(item);
+        clone.querySelector('img.pyramid-items').src = item.image;
+       
+        //apend
+        document.querySelector('.food-item-img').appendChild(clone);
+    });
+
+    /*const pyramidTemplate = document.querySelector('.food-pyramid-img').content;
+
+    gameOneData.forEach(item => {
+       
+        //clone it
+        const clone = pyramidTemplate.cloneNode(true); 
+        //change content
+        console.log(item);
+       clone.querySelector('.img').src = item.image;
+       
+        //append
+        document.querySelector('.food-item-img').appendChild(clone);
+    });*/
+  }
+
+    theBox.classList.add('scale-up');
+    boxText.classList.add('fade-out');
+    boxHeading.innerHTML = "";
+    boxBody.innerHTML = "";
+    document.querySelector('.pyramid-game-hidden').classList.remove('hidden');
+    theButton.innerHTML = 'Done';
+    theButton.addEventListener('click', () => {
+    document.querySelector('.pyramid-game-hidden').classList.add('hidden');
+    theBox.classList.remove('scale-up');
+        theButton.innerHTML = 'Finish';
+        boxHeading.innerHTML="Good job!"
+        boxBody.innerHTML="Select a plate to get a recipe."
+        document.querySelector('.plates').classList.remove('hidden');
+        document.querySelector('.step-two').classList.remove('inactive');
+    })
+}
 
 // recipes after the game
 // fetch recipes
