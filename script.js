6c7bac3380a4f845b6037454bd0dc39d957d1baa
Justina Jemeljanovaite <just1528@stud.kea.dk>
2022-01-26 12:07:11 +0100

    half-working pyramid

--- script.js
+++ script.js
@@ -43,8 +43,6 @@ function loadText(text) {
     function changeText() {
 
         if (i < 7) {
-
-        
         boxText.classList.add('fade-out');
         boxHeading.innerHTML = text[i].heading;
         boxBody.innerHTML = text[i].body;
@@ -62,7 +60,7 @@ function loadText(text) {
 }}
 
 
-function openGameOne(gameOneData) {
+function openGameOne() {
     
 // fetch food pyramid data
 fetch(`https://spring21-427e.restdb.io/rest/food-pyramid`, {
@@ -91,24 +89,73 @@ fetch(`https://spring21-427e.restdb.io/rest/food-pyramid`, {
         //change content
         console.log(item);
         clone.querySelector('img.pyramid-items').src = item.image;
+        clone.querySelector('img.pyramid-items').class = `pyramid-items draggable item-shelf-${item.shelf}`;
        
         //apend
         document.querySelector('.food-item-img').appendChild(clone);
     });
 
-    /*const pyramidTemplate = document.querySelector('.food-pyramid-img').content;
+    const draggableElements = document.querySelectorAll(".draggable");
+    const droppableElements = document.querySelectorAll(".droppable");
 
-    gameOneData.forEach(item => {
-       
-        //clone it
-        const clone = pyramidTemplate.cloneNode(true); 
-        //change content
-        console.log(item);
-       clone.querySelector('.img').src = item.image;
-       
-        //append
-        document.querySelector('.food-item-img').appendChild(clone);
-    });*/
+    draggableElements.forEach(elem => {
+      elem.addEventListener("dragstart", dragStart); // Fires as soon as the user starts dragging an item - This is where we can define the drag data
+      // elem.addEventListener("drag", drag); // Fires when a dragged item (element or text selection) is dragged
+      // elem.addEventListener("dragend", dragEnd); // Fires when a drag operation ends (such as releasing a mouse button or hitting the Esc key) - After the dragend event, the drag and drop operation is complete
+});
+
+droppableElements.forEach(elem => {
+  elem.addEventListener("dragenter", dragEnter); // Fires when a dragged item enters a valid drop target
+  elem.addEventListener("dragover", dragOver); // Fires when a dragged item is being dragged over a valid drop target, repeatedly while the draggable item is within the drop zone
+  elem.addEventListener("dragleave", dragLeave); // Fires when a dragged item leaves a valid drop target
+  elem.addEventListener("drop", drop); // Fires when an item is dropped on a valid drop target
+});
+
+// Drag and Drop Functions
+
+//Events fired on the drag target
+
+function dragStart(event) {
+  event.dataTransfer.setData("text", event.target.class); // or "text/plain" but just "text" would also be fine since we are not setting any other type/format for data value
+}
+
+//Events fired on the drop target
+
+function dragEnter(event) {
+  if(!event.target.classList.contains("dropped")) {
+    event.target.classList.add("droppable-hover");
+  }
+}
+
+function dragOver(event) {
+  if(!event.target.classList.contains("dropped")) {
+    event.preventDefault(); // Prevent default to allow drop
+  }
+}
+
+function dragLeave(event) {
+  if(!event.target.classList.contains("dropped")) {
+    event.target.classList.remove("droppable-hover");
+  }
+}
+
+function drop(event) {
+  event.preventDefault(); // This is in order to prevent the browser default handling of the data
+  event.target.classList.remove("droppable-hover");
+  const draggableElementData = event.dataTransfer.getData("text"); // Get the dragged data. This method will return any data that was set to the same type in the setData() method
+  const droppableElementData = event.target.getAttribute("data-draggable-class");
+  const isCorrectMatching = draggableElementData === droppableElementData;
+  if(isCorrectMatching) {
+    const draggableElement = document.getElementByClass(draggableElementData);
+    event.target.classList.add("dropped");
+    // event.target.style.backgroundColor = draggableElement.style.color; // This approach works only for inline styles. A more general approach would be the following: 
+    // event.target.style.backgroundColor = window.getComputedStyle(draggableElement).color;
+    draggableElement.classList.add("dragged");
+    draggableElement.setAttribute("draggable", "false");
+    event.target.insertAdjacentHTML("afterbegin", `<i class="fas fa-${draggableElementData}"></i>`);
+  }
+}
+
   }
 
     theBox.classList.add('scale-up');
@@ -160,3 +207,5 @@ function showRecipe(recipes) {
         theButton.addEventListener('click', toggleBox);
     })
 }
+
+//open theme 2
